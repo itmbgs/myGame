523fd77cfded1787cad1ab293415c0858fa10245
Ajeet Kumar <136183426+itmbgs@users.noreply.github.com>
2026-03-28 16:40:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,62 @@
-let button = document.querySelector(".btn");
-button.addEventListener("click", display)
-let hindiBox = document.querySelector(".hindi");
-let englishBox = document.querySelector(".english");
+    let hindiBox = document.querySelector(".hindi");
+    let englishBox = document.querySelector(".english");
 
-hindiBox.innerText = "";
-englishBox.innerText = "";
+    let hindiArray = ["अ", "आ", "इ", "ई", "उ", "ऊ", "ऋ",
+                    "ए ", "ऐ", "ओ", "औ", "अं", "अः",
+                    "क", "ख", "ग", "घ", "ङ",
+                    "च", "छ", "ज", "झ", "ञ",
+                    "ट", "ठ", "ड", "ढ", "ण",
+                    "त", "थ", "द", "ध", "न",
+                    "प", "फ", "ब", "भ", "म",
+                    "य", "र", "ल", "व", "श", "ष", "स", "ह",
+                    "क्ष", "त्र", "ज्ञ", "श्र",
+    ];
+    let englishArray = ["a", "aa", "i", "ee", "u", "oo", "ri",
+                    "e", "ai", "o", "au", "an", "ah",
+                    "k", "kh", "g", "gh", "ng",
+                    "ch", "chh", "j", "jh", "N",
+                    "t", "th", "d", "dh", "n",
+                    "t", "th", "d", "dh", "n",
+                    "p", "ph", "b", "bh", "m",
+                    "y", "r", "l", "w", "sh", "sh", "s", "h",
+                    "ksh", "tr", "gy", "sr",
+    ];
 
-let hindiArray = ["अ", "आ", "इ", "ई", "उ", "ऊ"];
-let englishArray = ["a", "aa", "i", "ee", "u", "oo"];
+    function getIndex(){
+        return Math.floor(Math.random()*hindiArray.length) ;
+    }
 
-function display() {
-    return new Promise((resolve, reject) => {
+let index = getIndex();
 
-        let index = Math.floor(Math.random()*6) ;
-        hindiBox.innerText = hindiArray[index];
-
-        setTimeout(()=>{
-            englishBox.innerText = englishArray[index];
-        }, 3000);
-        setTimeout(()=>{
-            hindiBox.innerText = "";
-            englishBox.innerText = "";
-            resolve();
-        }, 5000);
-    });  
+function startGame(){
+    hindiBox.innerText = "";
+    englishBox.innerText = "";
+    index = getIndex();
+    hindiBox.innerText = hindiArray[index];
 }
 
+function displayEnglish(){
+    if(hindiBox.innerText === ""){
+        englishBox.innerText = "";
+    }else{
+        englishBox.innerText = englishArray[index];
+    }
+    setTimeout(()=>{
+        hindiBox.innerText = "";
+        englishBox.innerText = "";
+    }, 2000);
+}
+
+startGame();
+
+let answerButton = document.querySelector(".answerButton");
+answerButton.addEventListener("click", displayEnglish);
+
+let playAgainButton = document.querySelector(".playAgain");
+playAgainButton.addEventListener("click", playAgain);
+
+function playAgain(){
+    if(hindiBox.innerText == ""){
+        startGame(index);
+    }
+}
